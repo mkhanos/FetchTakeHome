a6d593a52fd38693522ed250b2e5138eb7575279
Momo Khan <mkhanongm@gmail.com>
2025-05-30 14:34:47 -0400

    Load the list of recipes with their names and cuisine type

--- FetchTakeHome/ContentView.swift
+++ FetchTakeHome/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var recipes: [Recipe] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView() {
+            VStack(alignment: .leading) {
+                ForEach(recipes, id: \.uuid) { recipe in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(recipe.name)
+                                .font(.headline)
+                            Text(recipe.cuisine)
+                                .font(.subheadline)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .refreshable {
+            await loadData()
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        let url = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json")!
+        do {
+            // TODO: - Abstract network later
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let recipeResponse = try JSONDecoder().decode(RecipeResponse.self, from: data)
+            self.recipes = recipeResponse.recipes
+        } catch {
+            // TODO: - handle network failure
+            print("Failed")
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(recipes: [])
 }
